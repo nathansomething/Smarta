95e883a317d877a95ac3e785f089e1a0f8eb7d1f
Nathan Goodman <Nathan.something@gmail.com>
2018-02-11 07:12:05 -0500

    comitting

--- techvalley/server/SmartaServer.py
+++ techvalley/server/SmartaServer.py
@@ -8,14 +8,14 @@ api = Api(app)
 class SmartaSampleData(Resource):
 
     def get(self):
-        return get_sample_data.get_tfevt_assets()
+        return get_sample_data.get_tfevt_assets(), {'Access-Control-Allow-Origin': '*'}
 
 class SmartaDataForAssetId(Resource):
-    
+
     def get(self, userfriendly_id):
         print("got id={}".format(userfriendly_id))
-        return get_sample_data.get_vehicle_counts_for_asset_id(userfriendly_id)
-            
+        return get_sample_data.get_vehicle_counts_for_asset_id(userfriendly_id), {'Access-Control-Allow-Origin': '*'}
+
 api.add_resource(SmartaSampleData, '/')
 api.add_resource(SmartaDataForAssetId, '/<int:userfriendly_id>')
 
