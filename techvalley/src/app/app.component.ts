f9a991ce54a58d022bcecfbd15ed804c845b7949
Ilya Mayzus <imayzus@gmail.com>
2018-02-10 22:12:24 -0500

    added flask code

--- techvalley/src/app/app.component.ts
+++ techvalley/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+	ngOnInit() {
+	
+		function getToken() {
+		
+		
+		}
+	
+	
+	}
 }
+
+
